f1c05d97ab840f9545a06388730f55f57e30432b
httpcarlxs <56499493+httpcarlxs@users.noreply.github.com>
2020-10-31 23:18:47 -0300

    Update projeto.c

--- project/projeto.c
+++ project/projeto.c
@@ -3,7 +3,30 @@
 #include <locale.h>
 #include <string.h>
 
+void imprimir_tabuleiro(char tab[3][3]);
+
 int main()
 {
 	return 0;
+}
+
+void imprimir_tabuleiro(char tab[][3])
+{
+	int j, k;
+	for(j = 0; j < 3; j++)
+	{
+		for(k = 0; k < 3; k++)
+		{
+			if(tab[j][k] != 'X' && tab[j][k] != 'O')
+				printf("\t \t");
+			else
+				printf("\t%c\t", tab[j][k]);
+			
+			if(k < 2)
+				printf("|");
+		}
+		
+		if(j < 2)
+			printf("\n__________________________________________________\n\n");
+	}	
 }
